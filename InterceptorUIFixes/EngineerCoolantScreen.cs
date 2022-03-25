5ac64868eae60880174974be2aa9f4e16b1c67f7
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-03-24 23:10:06 -0700

    Add project files.

---
+++ InterceptorUIFixes/EngineerCoolantScreen.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InterceptorUIFixes
+{
+    [HarmonyPatch(typeof(PLEngineerCoolantScreen), "Update")]
+    class EngineerCoolantScreen
+    {
+        static void Postfix(PLEngineerCoolantScreen __instance, List<UISprite> ___CoolantPumpBtns)
+        {
+            if (__instance != null && __instance.MyScreenHubBase.OptionalShipInfo.ShipTypeID == EShipType.OLDWARS_HUMAN)
+            {
+                ___CoolantPumpBtns[__instance.MyScreenHubBase.OptionalShipInfo.ReactorCoolingPumpState].color = Color.white;
+            }
+        }
+    }
+}
---
+++ InterceptorUIFixes/Mod.cs
@@ -0,0 +1,20 @@
+﻿using PulsarModLoader;
+
+namespace InterceptorUIFixes
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.1";
+
+        public override string Author => "Dragon";
+
+        public override string Name => "InterceptorUIFixes";
+
+        public override string LongDescription => "Changes coolant button color to stand out.";
+
+        public override string HarmonyIdentifier()
+        {
+            return $"{Author}.{Name}";
+        }
+    }
+}
